5b8890dc98279e128d2c8a443c90dd77ed4c3c5a
Lucas Vinicius <lucasvdlk2017@gmail.com>
2023-02-14 15:20:49 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,45 @@
+function img1() {
+    document.getElementById("img-hero").src="image 2.png"
+}
+function img2() {
+    document.getElementById("img-hero").src="image 3.png"
+}
+function img3() {
+    document.getElementById("img-hero").src="image 5.png"
+}
+function img4() {
+    document.getElementById("img-hero").src="image 6.png"
+}
+function img5() {
+    document.getElementById("img-hero").src="image 1.png"
+}
+function img6() {
+    document.getElementById("img-hero").src="image 7.png"
+}
+function img7() {
+    document.getElementById("img-hero").src="image 8.png"
+}
+function img8() {
+    document.getElementById("img-hero").src="image 9.png"
+}
+function img9() {
+    document.getElementById("img-hero").src="image 10.png"
+}
+function img10() {
+    document.getElementById("img-hero").src="image 12.png"
+}
+function img11() {
+    document.getElementById("img-hero").src="image 13.png"
+}
+function img12() {
+    document.getElementById("img-hero").src="image 14.png"
+}
+function img13() {
+    document.getElementById("img-hero").src="image 15.png"
+}
+function img14() {
+    document.getElementById("img-hero").src="image 16.png"
+}
+function img15() {
+    document.getElementById("img-hero").src="image 17.png"
+}
